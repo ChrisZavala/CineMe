18f462a24aab5ace44fe66ed263c9ddb0ab5e89d
Chris Zavala <frappejcat@gmail.com>
2023-02-10 09:50:34 -0600

    finished the routes and now the server is up.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,8 +23,7 @@ router.get('/', async (req, res) => {
                 );
             }
         }
-
-        //here we are also making our api calls
+        //here we are also making our api calls to get everything popular and using the sites api to our advantage. 
         res.render("homepage", {
             popularMovies: getContentData[0].data.results,
             topMovies: getContentData[1].data.results,
@@ -34,7 +33,7 @@ router.get('/', async (req, res) => {
         res.render("404-page");
     }
 });
-
+//get /watchlist 
 router.get('/watchlist', async (req, res) => {
     if(!req.session.loggedIn) {
         res.redirect("/login");
@@ -43,8 +42,9 @@ router.get('/watchlist', async (req, res) => {
         res.redirect('/watchlist' + req.session.user_id);
     }
 });
-
+//get /watchlist/:id getting our status. Thanks to Rick for setting this in the models
 router.get('/watchlist/:id', async (req, res) => {
+    try{
     const db = await Promise.all([
         User.findByPk(req.params.id, { attributes: { exclude: ['password']}}),
         Watchlist.findAll({ where: { user_id: req.params, status: 0 }}),
@@ -55,5 +55,55 @@ router.get('/watchlist/:id', async (req, res) => {
         res.render('404-page', { message: "No id found!"});
         return; 
     }
-    
-})
+const currentUser = (req.session.loggedIn) ? req.session.user_id == req.params.id : false;
+let watchlist = [];
+for(let x=1; x<db.length; x++){
+    watchlist.push(db[x].map((entry) => entry.get({plain: true})));
+    for(let y=1; y<watchlist[x-1].length; y++){
+        watchlist[x-1][y].currentUser = currentUser
+    }
+}
+
+//going to handlebars page to render watchlist if they are this user or different user
+res.render('watchlist', {
+    futureWatch: watchlist[0],
+    currentlyWatching: watchlist[1],
+    finished: watchlist[2],
+    loggedIn: req.session.loggedIn,
+    currentUser
+});
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+//When the user is 'loggedIn' will go to the homepage
+//get /login
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    //if not let the user signup for account
+    res.render('Sign Up for Account!');
+});
+
+//Our search engine. 
+//get /search/:type/:query
+router.get('/search/:type/:query', async (req, res) => {
+    try {
+        const query = req.params.query.split('+').join("");
+        let type = req.params.type === "movie";
+        const searchDB = await searchContent(query, type);
+        for (let y=0; y<searchContent.data.results.length; y++){
+        }
+        res.render('search', {
+            searchContent: searchDB.data.results, loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.render('404-page');
+    }
+});
+
+//Export time thank god
+module.exports = router;
