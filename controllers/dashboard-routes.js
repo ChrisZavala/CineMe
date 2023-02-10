641f1c3fd48ff2d843368a2cea755a5e575c8fae
Chris Zavala <frappejcat@gmail.com>
2023-02-10 01:15:34 -0600

    creating the longest the home-routes.js.

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User } = require('../models/');
+const withAuth = require('../utils/auth');
+
+//get / async/await
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const dbCommentData = await Comment.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'comment_text',
+                'user_id',
+                'content_type',
+                'createdAt'
+            ],
+            include: [{ model: User, attributes: { exclude: ['password']}}]
+        });
+        const comments = dbCommentData.map(comment => comment.get({plain: true}));
+        const username = req.session.username;
+        const email = req.session.email;
+        res.render('dashboard', { comments, username, email, loggedIn: true}); 
+    } catch (err) {
+        res.status(500).json(err);
+        
+    }
+});
+module.exports = router; 
