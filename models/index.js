7fdf5fdb94aa85df15efbe66a151b1c71611dd21
Chris Zavala <frappejcat@gmail.com>
2023-02-11 20:12:14 -0600

    added the Comment and Poll to the index table in the models section.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,26 @@
 // import models
 const User = require('./User');
 const Watchlist = require('./Watchlist');
+const Comment = require('./Comment');
+const Poll = require('./poll');
+
+User.hasMany(Poll, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Poll.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
 User.hasMany(Watchlist, {
     foreignKey: 'user_id'
@@ -11,4 +31,4 @@ Watchlist.belongsTo(User, {
     onDelete: 'CASCADE'
 });
 
-module.exports = { User, Watchlist };
+module.exports = { User, Watchlist, Comment, Poll };
