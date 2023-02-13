1c41998c4dbffcf0d8990010c170a8dffd6330ae
Chris Zavala <frappejcat@gmail.com>
2023-02-13 16:41:45 -0600

    fixed the poll to and and keep pickles.

--- models/user.js
+++ models/user.js
@@ -46,7 +46,7 @@ User.init(
         pfp_path: {
         type: DataTypes.STRING,
         allowNull: false,
-        defaultValue: 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'
+        defaultValue: 'https://www.clipartmax.com/png/small/8-87550_free-to-use-popcorn-clip-art-movie-popcorn-clip-art.png'
         }
     },
     {
