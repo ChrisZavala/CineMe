e57de441c22ceb36337f747f4067aa5bd1c85ca4
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-02-13 12:45:20 -0600

    new

---
+++ main/public/styles.js
@@ -0,0 +1,11 @@
+    $(document).ready(function(){
+      $('.carousel').slick({
+          draggable: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          infinite: true,
+          slidesToShow: 11,
+          slidesToScroll: 1,
+          touchThreshold: 1000,
+      });
+    });
